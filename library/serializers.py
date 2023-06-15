0be900c160d23b0eb8c7e0c46cbbd6f9177d1435
BingBong <angusroymcdonald@gmail.com>
2023-06-15 15:51:22 +1000

    serializers - first commit

--- library/serializers.py
+++ library/serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
 from .models import Student, Librarian, Book, Borrowing
 
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+class LibrarianSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Librarian
+        fields = '__all__'
+
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
       model = Book
       fields = '__all__'
 
 
-class StudentSerializer(serializers.ModelSerializer):
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = '__all__'
